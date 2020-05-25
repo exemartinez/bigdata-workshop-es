669596853fba09be67df637f45a08cca4b9aa769
Ezequiel H. Martinez <hernanezequielmartinez@gmail.com>
2020-05-24 22:29:39 -0400

    Saved as parquet in stages

--- code/gw/generate_gw_dataset.py
+++ code/gw/generate_gw_dataset.py
@@ -23,42 +23,57 @@ hf = h5py.File(hd5f_file, 'r')
 #first, we get the dataframe in pandas with all the headers for the data.
 csv = '/dataset/trainingset_v1d1_metadata.csv'
 parquet='/dataset/gw_gravity_spy_dataframe.parquet'
+
 df=pd.read_csv(csv, sep=',',header=0)
-
-#forming a new dataset
-gwdf = df
-
-gvtyid = 21
-labelid = 22
-sampleid=23
-
-gwdf.shape[0]
-gwdf['png'] = ''
-
-print("The shape of the matrix to be processed is %s" % str(gwdf.shape))
-
-for index, record in gwdf.iterrows():
-    gravityspy_id= record[gvtyid]
-    label= record[labelid]
-    sample_type= record[sampleid]
-    
-    png = np.array(hf[label][sample_type][gravityspy_id]['0.5.png'][0])
-    
-    png = np.reshape(png,23800).tolist()  #we place the whole image in just one dimension array. 140x170 (remember this)
-    
-    gwdf.at[index, 'png'] = png
-
-    print("Record #%s processed" % str(index))
-
-print("Main procedures finished. Saving...")
 
 findspark.init()
 spark = SparkSession.builder.appName("pyspark-gw").getOrCreate()
-
 sqlCtx = SQLContext(spark)
-sdf = sqlCtx.createDataFrame(gwdf) #getting a spark dataframe out of a Pandas dataframe.   
 
-# We use the spark dataset to write its contents (all the gravity spy's dataset) to a partquet file for easy classification. 
-sdf.write.parquet(parquet)
+#forming a new dataset - for everyblock block
+block_number = 1
+block = 1138
+begin = 0
+end = block * block_number
+all_records = 7966
 
-print("File saved as parquet." + parquet)
+for block_init in range(begin, all_records, block): # will process 7 different blocks
+
+    gwdf = df[block_init:block_init+block] #we reduce the set of data to test the algorithm at a fast rate.
+
+    print("Processing block {0} to {1}...as parquet.".format(block_init, block_init + block))
+
+    gvtyid = 21
+    labelid = 22
+    sampleid=23
+
+    gwdf.shape[0]
+    gwdf['png'] = ''
+
+    print("The shape of the matrix to be processed is %s" % str(gwdf.shape))
+
+    for index, record in gwdf.iterrows():
+        gravityspy_id= record[gvtyid]
+        label= record[labelid]
+        sample_type= record[sampleid]
+        
+        png = np.array(hf[label][sample_type][gravityspy_id]['0.5.png'][0])
+        
+        png = np.reshape(png,23800).tolist()  #we place the whole image in just one dimension array. 140x170 (remember this)
+        
+        gwdf.at[index, 'png'] = png
+
+        print("Record #%s processed" % str(index))
+
+    print("Main procedures finished. Saving block...")
+
+    sdf = sqlCtx.createDataFrame(gwdf) #getting a spark dataframe out of a Pandas dataframe.   
+
+    # We use the spark dataset to write its contents (all the gravity spy's dataset) to a partquet file for easy classification. 
+    sdf.write.format("parquet") \
+    .partitionBy("gravityspy_id","sample_type") \
+    .option("path", "/dataset/gw_gravity_spy_dataframe_" + block_init) \
+    .mode("overwrite") \
+    .saveAsTable("gw_gravity_spy" + block_init)    
+    
+    print("File saved block {0} to {1}...as parquet.".format(block_init, block_init + block))
